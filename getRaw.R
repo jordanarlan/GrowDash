71d8c71c1a108f7b5d5735b7fead45097e9e026e
jordanarlan <42752041+jordanarlan@users.noreply.github.com>
2019-04-24 15:00:54 -0500

    Changed to use just the sheet

--- getRaw.R
+++ getRaw.R
@@ -3,6 +3,7 @@ getRaw <- function () {
     url("https://docs.google.com/spreadsheets/d/e/2PACX-1vQgUGw6qAQCb0Z1k77WA1aqPw3j9RZepOaURCg_CxIBZXDCRk3BjRBdfNkl21se2_kpDOglRtG9w6u_/pub?gid=0&single=true&output=csv"),
     strip.white = TRUE
   )
-  data$date = as.POSIXct(data$timestamp, tz="GMT", origin="1970-01-01")
+  data$Time = as.POSIXct(data$Time, tz="GMT", origin="1970-01-01")
   data
 }
+
